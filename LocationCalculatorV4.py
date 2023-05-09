corrected a negative sign leading to significant position error and got the algorythm validation script (v4b) working

LocationCalculatorV4.py
@@ -96,7 +96,7 @@ X = np.linalg.solve(A,B)
 #convert cartesian to lat/lon
 x, y, z = X[0], X[1], X[2]
 Lat = Latitude( np.arctan2( z, np.hypot(x,y) )[0]*u.rad, unit = u.deg )
-Lon = Longitude( np.arctan2( y, x )[0]*u.rad, unit = u.deg, wrap_angle = 180*u.deg )
+Lon = -Longitude( np.arctan2( y, x )[0]*u.rad, unit = u.deg, wrap_angle = 180*u.deg )
 
 #### Output
 

LocationCalculatorV4B.py
@@ -3,7 +3,6 @@
 # Copyright Ulnooweg Education Centre, 2023, All rights reserved
 ###############################################################################
 
-# git test comment
 
 #### import packages
 import numpy as np
@@ -12,9 +11,12 @@ from numpy import genfromtxt
 from astropy.time import *
 from astropy.coordinates import *
 from astropy import units as u
+from prettytable import PrettyTable
 
 
-#### Load Navigational Stars
+###############################################################################
+# Load Navigational Stars
+###############################################################################
 
 # Read CSV of navigational stars
 NavStars = genfromtxt( 'NavigationalStars.csv', delimiter=',' ) # read the star list csv
@@ -23,7 +25,9 @@ NavStars = np.delete( NavStars, (0,1,2), axis=1) # delete the name collumns
 #row now corresponds to almanac number with the addition of 0 = polaris
 
 
-#### Define Functions
+###############################################################################
+# Define Functions
+###############################################################################
 
 # fn that creates a list of test cases
 def makeTestCases(numCases):
@@ -37,7 +41,7 @@ def makeTestCases(numCases):
     return TestCases
 
 # fn that creates test observations from the test cases
-def getTestObservations(TestCases,time):
+def getTestObservations(TestCases,time,round2minutes):
     
     numObservationsPerLocation = 3
     numTestCases = len(TestCases)
@@ -65,11 +69,16 @@ def getTestObservations(TestCases,time):
             Star = Star.transform_to(testFrame)
             alt  = Star.alt
             
+            # accounting for precision of measurement
+            if round2minutes == True:
+                alt = Angle(alt.to_string(unit=u.deg, fields = 2),unit=u.deg)
+                print(alt)
+            
             ERA = Time.earth_rotation_angle( time, longitude = 0) # earth rotation angle at the observation time
             SHA = Longitude( 360 - ra, unit = u.deg ) # sideral hour angle from right ascention (from csv)
             GHA = Longitude( ERA + SHA, unit = u.deg ) # greenwich hour angle 
             dec = Latitude( dec, unit = u.deg ) # declination from table
-        
+            
             observations[i][k] = [testStar, time, alt, GHA, dec]
 
     return observations
@@ -87,17 +96,38 @@ def obs2RowOfA(observation):
 
     return A_row
 
+# haversine function for error calculation
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
 
-#### Run Tests
+    All args must be of equal length.    
 
-numCases = 60
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+
+###############################################################################
+# Run Tests
+###############################################################################
+
+numCases = 10
+round2minutes = True
 TestCases = makeTestCases(numCases)
-TestObservations = getTestObservations(TestCases,Time.now()) ##### this one is fucked for some reason
+TestObservations = getTestObservations(TestCases,Time.now(),round2minutes)
 
-print(TestObservations)
 
-#### Calculate Position
-DetPos = [ [None, None] ]*numCases
+# Calculate Position
+DetPos = [None for i in range(numCases)]
 for i in range(numCases): #for every test case
     A = np.zeros( [3,3] )
     B = np.ones( (3,1) )
@@ -109,9 +139,31 @@ for i in range(numCases): #for every test case
     X = np.linalg.solve(A,B)
     x, y, z = X[0], X[1], X[2]
     Lat = Latitude( np.arctan2( z, np.hypot(x,y) )[0]*u.rad, unit = u.deg )
-    Lon = Longitude( np.arctan2( y, x )[0]*u.rad, unit = u.deg, wrap_angle = 180*u.deg )
-    DetPos[i] = [ [Lat, Lon] ]
+    Lon = -Longitude( np.arctan2( y, x )[0]*u.rad, unit = u.deg, wrap_angle = 180*u.deg )
+    DetPos[i] = EarthLocation.from_geodetic(Lon,Lat,0,'WGS84')
 
-#### print outputs
+# Calculate error
+posError = [None for i in range(numCases)]
 for i in range(numCases):
-    print('Test Case ',i+1,' Determined Position: ',DetPos[i],'Actual Position: ',TestCases[i])
+    trueLon = TestCases[i].lon.rad
+    trueLat = TestCases[i].lat.rad
+    detLon = DetPos[i].lon.rad
+    detLat = DetPos[i].lat.rad
+    posError[i] = haversine_np(trueLon, trueLat, detLon, detLat)
+
+###############################################################################
+# Output results
+###############################################################################
+
+print('\n Position Determination Simulated Test Results:\n')
+headers = ['Test Case','Determined Latitude','True Latitude', 'Determined Longitude', 'True Longitude', 'Total Position Error [km]']
+table = PrettyTable(headers)
+for i in range(numCases):
+    DetLatStr = DetPos[i].lat.to_string( unit=u.deg, decimal=False, sep='dms', precision=0)
+    TruLatStr = TestCases[i][0].lat.to_string( unit=u.deg, decimal=False, sep='dms', precision=0)
+    DetLonStr = DetPos[i].lon.to_string( unit=u.deg, decimal=False, sep='dms', precision=0)
+    TruLonStr = TestCases[i][0].lon.to_string( unit=u.deg, decimal=False, sep='dms', precision=0)
+    PosErrStr = str(round(posError[i][0],3)) # round position error to nearest meter
+    table.add_row( [ i, DetLatStr, TruLatStr, DetLonStr, TruLonStr, PosErrStr] )
+
+print(table)