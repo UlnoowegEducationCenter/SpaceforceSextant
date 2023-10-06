31e8b8d6558b8ce7369a92ba29c93fa3a83a7a54
c-seymour-UEC <97196281+c-seymour-UEC@users.noreply.github.com>
2023-10-06 11:23:17 -0300

    Delete ephemapp directory

--- ephemapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-import datetime
-import pytz
-
-class SextantReadingForm(forms.Form):
-    longitude = forms.CharField(required= 'False',label='longitude')
-    latitude = forms.CharField(label='latitude')
-    date_time = forms.CharField(label='date_time')
